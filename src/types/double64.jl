2f8344c3d870e031049ae846ddd9ad7ae08b3cf8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-13 17:32:08 -0400

    Create double64.jl

---
+++ src/types/double64.jl
@@ -0,0 +1,26 @@
+const TwoTupleF64 = Tuple{Float64, Float64}
+const TwoTupleC64 = Tuple{Complex{Float64}, Complex{Float64}}
+
+struct FloatD64 <: Real
+    val::TwoTupleF64
+end
+
+struct ComplexD64 <: Complex
+    val::TwoTupleC64
+end
+
+hi(x::FloatD64) = x.val[1]
+lo(x::FloatD64) = x.val[2]
+hilo(x::FloatD64) = x.val
+
+hi(x::ComplexD64) = x.val[1]
+lo(x::ComplexD64) = x.val[2]
+hilo(x::ComplexD64) = x.val
+
+hi(x::Float64) = x
+lo(x::Float64) = 0.0
+hilo(x::Float64) = (x, 0.0)
+
+hi(x::Complex{Float64}) = x
+lo(x::Complex{Float64}) = 0.0+0.0im
+hilo(x::Complex{Float64}) = (x, 0.0+0.0im)
