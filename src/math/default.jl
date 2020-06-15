923c5a106dd326533277fe6b45b484f1e8cf2599
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-15 10:42:47 -0400

    Update default.jl

--- src/math/default.jl
+++ src/math/default.jl
@@ -3,8 +3,8 @@ for F in (:abs, :sqrt, :cbrt,
           :acos, :acosh, :asin, :asinh, :atan, :atanh,
           :cos, :cosh, :sin, :sincos, :sinh, :tan, :tanh,
           :ceil, :floor, :trunc, :round,)
-   @eval $F(x::Real) = FloatD64($F(Float128(x)))
+   @eval Base.Math.$F(x::Real) = FloatD64($F(Float128(x)))
 end
 
-Base.Math.modf(x::R) where {R<:Real} = FloatD64.(modf(Float128(x)))
-Base.Math.hypot(x::R, y::R) where {R<:Real} = FloatD64(hypot(Float128(x), Float128(y)))
+Base.Math.modf(x::R) where {R} = FloatD64.(modf(Float128(x)))
+Base.Math.hypot(x::R, y::R) where {R} = FloatD64(hypot(Float128(x), Float128(y)))
