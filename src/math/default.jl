2a1f51bba304ab44be49e211f5694989d4452fe2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-06-15 10:49:30 -0400

    Update default.jl

--- src/math/default.jl
+++ src/math/default.jl
@@ -1,12 +1,21 @@
-import Base.Math: abs, sqrt, cbrt, exp, expm1, log, log10, log1p, log2, ceil, floor, truc, round
+for F in (:abs, :sqrt, :cbrt,
+          :exp, :expm1, :log, :log10, :log1p, :log2,
+          :acos, :acosh, :asin, :asinh, :atan, :atanh,
+          :cos, :cosh, :sin, :sincos, :sinh, :tan, :tanh,
+          :ceil, :floor, :trunc, :round,)
+   @eval Base.Math.$F(x::FloatD64) = FloatD64($F(Float128(x)))
+end
+
+Base.Math.modf(x::FloatD64) = FloatD64.(modf(Float128(x)))
+Base.Math.hypot(x::FloatD64, y::FloatD64) = FloatD64(hypot(Float128(x), Float128(y)))
 
 for F in (:abs, :sqrt, :cbrt,
           :exp, :expm1, :log, :log10, :log1p, :log2,
           :acos, :acosh, :asin, :asinh, :atan, :atanh,
           :cos, :cosh, :sin, :sincos, :sinh, :tan, :tanh,
           :ceil, :floor, :trunc, :round,)
-   @eval Base.Math.$F(x::Real) = FloatD64($F(Float128(x)))
+   @eval Base.Math.$F(x::ComplexD64) = ComplexD64($F(Complex{Float128}(x)))
 end
 
-Base.Math.modf(x::R) where {R} = FloatD64.(modf(Float128(x)))
-Base.Math.hypot(x::R, y::R) where {R} = FloatD64(hypot(Float128(x), Float128(y)))
+Base.Math.modf(x::ComplexD64) = ComplexD64.(modf(Complex{Float128}(x)))
+Base.Math.hypot(x::ComplexD64, y::ComplexD64) = ComplexD64(hypot(Complex{Float128}(x), Complex{Float128}(y)))
