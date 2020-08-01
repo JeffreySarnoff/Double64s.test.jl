b856a3bb63097f7afafd9a5083434d48fbe2d98c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-01 11:41:13 -0400

    Create prearith.jl

---
+++ src/math/prearith.jl
@@ -0,0 +1,36 @@
+Base.:(-)(x::FloatD64) = FloatD64((-Hi(x), -Lo(x)))
+Base.abs(x::FloatD64) = signbit(x) ? -x : x
+Base.abs2(x::FloatD64) = x*x
+Base.copysign(x::FloatD64, y) = signbit(y) ? -abs(x) : abs(x)
+Base.flipsign(x::FloatD64, y) = signbit(y) ? -x : x
+
+function Base.ceil(x::FloatD64)
+    xhi = Hi(x)
+    hi = ceil(Hi(x))
+    if hi === xhi
+        t = ceil(Lo(x))
+        hi = hi + t
+        lo = t - (hi - xhi)
+    else
+        lo = 0.0
+    end
+    return FloatD64((hi, lo))
+end
+
+function Base.floor(x::FloatD64)
+    xhi = Hi(x)
+    hi = floor(Hi(x))
+    if hi === xhi
+        t = floor(Lo(x))
+        hi = hi + t
+        lo = t - (hi - xhi)
+    else
+        lo = 0.0
+    end
+    return FloatD64((hi, lo))
+end
+
+function Base.trunc(x::FloatD64)
+    return signbit(x) ? ceil(x) : floor(x)
+end
+
