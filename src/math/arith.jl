144f794a22a91e47571213b05b2f14fd5ff3dec6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-01 17:35:08 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -38,31 +38,54 @@ end
 # relative error < 9.8u², 31 FP Ops, 100.7 bits (relative)
 # Algorithm 18 from [Joldes, Muller, Popescu 2017] 
 function Base.:(/)(x::FloatD64, y::FloatD64)
-    negihi = -inv(Hi(y))
-    rhi = fma(Hi(y), negihi, 1.0) 
-    rlo = mul(Lo(y), ihi)
-    hi, lo = two_hilo_sum(rhi, rlo)
-    hi = mul(hi, ihi)
-    lo = mul(lo, ihi)
-    hi = add(hi, ihi)
-    lo = add(lo, ihi)
-    hi = mul(x, hi)
-    lo = mul(x, lo)
-    return FloatD64((hi, lo))
+    yhi, ylo = HiLo(y)
+    th = inv(yhi)
+    rh = fma(-yhi, th, 1.0)
+    rl = -ylo * th
+    eh, el = two_hilo_sum(rh, rl)
+    dh, dl = DWTimesFP3(eh, el, th) # (eh, el) * rh
+    mh, ml = DWPlusFP(dh, dl, th) # (dh, dl) + th
+    zh, zl = DWTimesDW2(Hi(x), Lo(x), mh, ml) # (xh, xl) * (mh, ml)
+    return FloatD64(zh, zl)
+end
+
+# algorithm 9 from [Joldes, Muller, Popescu 2017] 
+@inline function DWTimesFP3(xh, xl, y)
+    ch, cl1 = two_prod(xh, y)
+    cl3 = fma(xl, y, cl1)
+    zh, zl = two_hilo_sum(ch, cl3)
+    return zh, zl
+end
+# algorithm 4 from [Joldes, Muller, Popescu 2017] 
+@inline function DWPlusFP(xh::Float64, xl::Float64, y::Float64)
+    sh, sl = two_sum(xh, y)
+    v = xl + sl
+    zh, zl = two_hilo_sum(sh, v)
+    return zh, zl
+end
+# algorithm 12 from [Joldes, Muller, Popescu 2017] 
+@inline function DWTimesDW2(xh, xl, yh, yl)
+    ch, cl1 = two_prod(xh, yh)
+    tl0 = xl * yl
+    tl1 = fma(xh, yl, tl0)
+    cl2 = fma(xl, yh, tl1)
+    cl3 = cl1 + cl2
+    zh, zl = two_hilo_sum(ch, cl3)
+    return zh, zl
 end
 
 # relative error < 9.8u², 31 FP Ops, 100.7 bits (relative)
 # Algorithm 18 from [Joldes, Muller, Popescu 2017] 
 function Base.:(inv)(y::FloatD64)
-    negihi = -inv(Hi(y))
-    rhi = fma(Hi(y), negihi, 1.0) 
-    rlo = mul(Lo(y), ihi)
-    hi, lo = two_hilo_sum(rhi, rlo)
-    hi = mul(hi, ihi)
-    lo = mul(lo, ihi)
-    hi = add(hi, ihi)
-    lo = add(lo, ihi)
-    return FloatD64((hi, lo))
+    yhi, ylo = HiLo(y)
+    th = inv(yhi)
+    rh = fma(-yhi, th, 1.0)
+    rl = -ylo * th
+    eh, el = two_hilo_sum(rh, rl)
+    dh, dl = DWTimesFP3(eh, el, th) # (eh, el) * rh
+    mh, ml = DWPlusFP(dh, dl, th) # (dh, dl) + th
+    # zh, zl = DWTimesDW2(1.0, 0.0, mh, ml) # (1, 0) * (mh, ml)
+    return FloatD64(mh, ml)
 end
 
 Base.:(\)(x::FloatD64, y::FloatD64) = (/)(y, x)
