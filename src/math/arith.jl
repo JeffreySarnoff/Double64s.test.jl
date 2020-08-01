ee3a70b8d8eda85b69fc22b518ef0507029f6dbb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-01 16:17:10 -0400

    Update arith.jl

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -26,14 +26,14 @@ end
 for T in (:FloatD64,)
   @eval begin  
     function neg(x::$T)
-        return $T(-hi(x), -lo(x))
+        return $T(-Hi(x), -Lo(x))
     end
     
     # relative error < 3u², 20 FP Ops
     # Algorithm 6 from [Joldes, Muller, Popescu 2017]
     function add(x::$T, y::$T)
-        hi, lo   = two_sum(hi(x), hi(y))
-        thi, tlo = two_sum(lo(x), lo(y))
+        hi, lo   = two_sum(Hi(x), Hi(y))
+        thi, tlo = two_sum(Lo(x), Lo(y))
         c = add(lo, thi)
         hi, lo = two_hilo_sum(hi, c)
         c = add(tlo, lo)
@@ -45,8 +45,8 @@ for T in (:FloatD64,)
     # Algorithm 6 from [Joldes, Muller, Popescu 2017]
     # reworked for subtraction
     function sub(x::$T, y::$T)
-        hi, lo   = two_diff(hi(x), hi(y))
-        thi, tlo = two_diff(lo(x), lo(y))
+        hi, lo   = two_diff(Hi(x), Hi(y))
+        thi, tlo = two_diff(Lo(x), Lo(y))
         c = add(lo, thi)
         hi, lo = two_hilo_sum(hi, c)
         c = add(tlo, lo)
@@ -57,10 +57,10 @@ for T in (:FloatD64,)
     # relative error < 5u², 9 FP Ops
     # Algorithm 12 from [Joldes, Muller, Popescu 2017]
     function mul(x::$T, y::$T)
-        hi, lo = two_prod(hi(x), hi(y))
-        t = mul(lo(x), lo(y))
-        t = fma(hi(x), lo(y), t)
-        t = fma(lo(x), hi(y), t)
+        hi, lo = two_prod(Hi(x), Hi(y))
+        t = mul(Lo(x), Lo(y))
+        t = fma(Hi(x), Lo(y), t)
+        t = fma(Lo(x), Hi(y), t)
         t = add(lo, t)
         hi, lo = two_hilo_sum(hi, t)
         return (hi, lo)
@@ -69,9 +69,9 @@ for T in (:FloatD64,)
     # relative error < 9.8u², 31 FP Ops
     # Algorithm 18 from [Joldes, Muller, Popescu 2017] 
     function divide(x::$T, y::$T)
-        negihi = -inv(hi(y))
-        rhi = fma(hi(y), negihi, 1.0) 
-        rlo = mul(lo(y), ihi)
+        negihi = -inv(Hi(y))
+        rhi = fma(Hi(y), negihi, 1.0) 
+        rlo = mul(Lo(y), ihi)
         hi, lo = two_hilo_sum(rhi, rlo)
         hi = mul(hi, ihi)
         lo = mul(lo, ihi)
