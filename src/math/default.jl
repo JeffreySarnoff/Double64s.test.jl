af0bdf4500211197fff86f2ee02b122489e0db52
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-01 13:11:44 -0400

    Update default.jl

--- src/math/default.jl
+++ src/math/default.jl
@@ -13,7 +13,7 @@ for F in (:sqrt, :cbrt,
           :exp, :expm1, :log, :log10, :log1p, :log2,
           :acos, :acosh, :asin, :asinh, :atan, :atanh,
           :cos, :cosh, :sin, :sincos, :sinh, :tan, :tanh,
-          :ceil, :floor, :trunc, :round,)
+          :round,)
    @eval Base.Math.$F(x::ComplexD64) = ComplexD64($F(Complex{Float128}(x)))
 end
 
