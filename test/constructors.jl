34266b861c960721a4e873b03c9da1ede83ac7a6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-01 16:18:51 -0400

    Update constructors.jl

--- test/constructors.jl
+++ test/constructors.jl
@@ -1,10 +1,10 @@
 @testset "FloatD64(x)" begin
-  @test hi(FloatD64(1.0)) === 1.0
-  @test lo(FloatD64(1.0)) === 0.0
-  @test hilo(FloatD64(1.0)) === (1.0, 0.0)
+  @test Hi(FloatD64(1.0)) === 1.0
+  @test Lo(FloatD64(1.0)) === 0.0
+  @test Hio(FloatD64(1.0)) === (1.0, 0.0)
   @test FloatD64(1.0e7, 1.0e-7) === FloatD64(1.00000000000001e7, -5.828380584716842e-10)  
-  @test hilo(FloatD64(1.0e7, 1.0e-7)) === (1.00000000000001e7, -5.828380584716842e-10)  
-  @test hilo(FloatD64((1.0e7, 1.0e-7))) === (1.0e7, 1.0e-7)
+  @test HiLo(FloatD64(1.0e7, 1.0e-7)) === (1.00000000000001e7, -5.828380584716842e-10)  
+  @test HiLo(FloatD64((1.0e7, 1.0e-7))) === (1.0e7, 1.0e-7)
   @test FloatD64(BigFloat(pi)) === FloatD64((3.141592653589793, 1.2246467991473532e-16))
 end
 
