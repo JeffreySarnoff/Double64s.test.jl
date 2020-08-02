14c27cff89a43f9dcd94ab47434b96eaf1e7774b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-02 06:27:17 -0400

    Update double64.jl

--- src/types/double64.jl
+++ src/types/double64.jl
@@ -11,18 +11,22 @@ const FloatComplexD64 = Union{FloatD64, ComplexD64}
 Hi(x::Float64) = x
 Lo(x::Float64) = 0.0
 HiLo(x::Float64) = (x, 0.0)
+ReIm(x::Float64) = (x, 0.0)
 
-Hi(x::Complex{Float64}) = x
-Lo(x::Complex{Float64}) = 0.0+0.0im
-HiLo(x::Complex{Float64}) = (x, 0.0+0.0im)
+Hi(x::ComplexF64) = x
+Lo(x::ComplexF64) = 0.0+0.0im
+HiLo(x::ComplexF64) = (x, 0.0+0.0im)
+ReIm(x::ComplexF64) = (real(x), imag(x))
 
 Hi(x::FloatD64) = x.val[1]
 Lo(x::FloatD64) = x.val[2]
 HiLo(x::FloatD64) = x.val
+ReIm(x::FloatD64) = (x, zero(FloatD64))
 
 Hi(x::ComplexD64) = x.val[1]
 Lo(x::ComplexD64) = x.val[2]
 HiLo(x::ComplexD64) = x.val
+ReIm(x::ComplexD64) = (real(x), imag(x))
 
 Base.real(x::FloatD64) = x
 Base.imag(x::FloatD64) = FloatD64((0.0, 0.0))
