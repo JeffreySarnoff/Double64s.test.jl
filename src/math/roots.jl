2d4d1be283cffd23f348a8dd7fa51f8e98eac3e0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-02 01:27:59 -0400

    Create roots.jl

---
+++ src/math/roots.jl
@@ -0,0 +1,30 @@
+onethird(::Type{Float64}) = 0.3333333333333333
+onethird(::Type{FloatD64}) = FloatD64((0.3333333333333333, 1.850371707708594e-17))
+
+function Base.sqrt(x::FloatD64)
+    x0 = sqrt(Hi(x))
+    x1 = (x0 + x/x0) * 0.5
+    x2 = (x1 + x/x1) * 0.5
+    return x2
+end
+ 
+function fastsqrt(x::FloatD64)
+    x0 = sqrt(Hi(x))
+    x1 = (x0 + x/x0) * 0.5
+    return x1
+end
+
+function Base.cbrt(x::FloatD64)
+    x0 = cbrt(Hi(x))
+    x1 = (2*x0 + x/(x0*x0)) / 3.0
+    x2 = (2*x1 + x/(x1*x1)) / 3.0
+    x3 = (2*x2 + x/(x2*x2)) / 3.0
+    return x3
+end
+ 
+function fastcbrt(x::FloatD64)
+    x0 = cbrt(Hi(x))
+    x1 = (2*x0 + x/(x0*x0)) / 3.0
+    x2 = (2*x1 + x/(x1*x1)) / 3.0
+    return x2
+end
