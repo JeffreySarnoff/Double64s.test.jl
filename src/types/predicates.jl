ad7643cad53d6349806643d59da8e821af833050
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-02 09:36:51 -0400

    Update predicates.jl

--- src/types/predicates.jl
+++ src/types/predicates.jl
@@ -3,10 +3,16 @@ for T in (:FloatD64, :ComplexD64)
     Base.isnan(x::$T) = isnan(Hi(x))
     Base.isinf(x::$T) = isinf(Hi(x))
     Base.isfinite(x::$T) = isfinite(Hi(x))
-    Base.issubnormal(x::$T) = issubnormal(Hi(x))
     Base.iszero(x::$T) = iszero(Hi(x))
     Base.isone(x::$T) = isone(Hi(x)) && iszero(Lo(x))
     Base.isinteger(x::$T) = isinteger(Hi(x)) && isinteger(Lo(x))
   end
 end
+
+Base.issubnormal(x::FloatD64) = issubnormal(Hi(x))
+
+Base.floatmin(::Type{FloatD64}) = FloatD64(floatmin(Float64))
+Base.floatmax(::Type{FloatD64}) = FloatD64(floatmax(Float64))
+Base.typemin(::Type{FloatD64}) = FloatD64(typemin(Float64))
+Base.typemax(::Type{FloatD64}) = FloatD64(typemax(Float64))
 
