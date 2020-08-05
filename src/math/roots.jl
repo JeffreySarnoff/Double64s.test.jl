8182255816a2444d9f963a58b02cc999b9b62b22
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-05 17:39:10 -0400

    Update roots.jl

--- src/math/roots.jl
+++ src/math/roots.jl
@@ -14,20 +14,12 @@ end
     return x1
 end
 
-function root3(x::FloatD64)
+function Base.cbrt(x::FloatD64)
     x0 = FloatD64(cbrt(Hi(p)))
     x1 = (x0 + fastsqrt(((4*p/x0)-x0^2)/3))/2
     return x1
 end
 
-function Base.cbrt(x::FloatD64)
-    x0 = FloatD64(cbrt(Hi(x)))
-    x1 = (2*x0 + x/(x0*x0)) / 3.0
-    x2 = (2*x1 + x/(x1*x1)) / 3.0
-    x3 = (2*x2 + x/(x2*x2)) / 3.0
-    return x3
-end
- 
 function fastcbrt(x::FloatD64)
     x0 = cbrt(Hi(x))
     x1 = (2*x0 + x/(x0*x0)) / 3.0
