b0c42af238485d391d1797490d8f31b151af80e7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-06 14:07:19 -0400

    Update lowlevel.jl

--- src/support/lowlevel.jl
+++ src/support/lowlevel.jl
@@ -10,3 +10,20 @@ fmod(parts::Tuple{FloatD64, FloatD64}) = fmod(parts...)
 function fmod(frc_part::FloatD64, int_part::FloatD64)
     return int_part + frc_part
 end
+
+const log10of2 = log10(2)
+const log2of10 = log2(10)
+
+@inline digits2bits(x::Int) = floor(Int, log2of10*x)
+@inline bits2digits(x::Int) = ceil(Int, log10of2*x)
+
+# fractionalbits(x) is the smallest number of bits such that
+# frac(x) = modf(x)[1]
+# isinteger(frac(x) * 2.0^fractionalbits(frac(x)))
+# fix for 0.0 and powers of 2 
+function fractionalbits(x::Float64)
+    fr = modf(x)[1]
+    nbits = 53 - trailing_zeros((reinterpret(UInt64, fr) & 0x000f_ffff_ffff_ffff))
+    return nbits
+end
+
