aa2ce710feed2df29fbec896beebb095a5ab606d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-08-06 02:52:13 -0400

    Update Double64s.jl

--- src/Double64s.jl
+++ src/Double64s.jl
@@ -16,13 +16,6 @@ using ErrorfreeArithmetic, Quadmath
 
 # online help text for the types
 
-"""
-    FloatD64
-
-A struct wrapping a Tuple of two Float64s: (most significant part, least significant part).
-
-Also the constructor for that struct.
-""" FloatD64
 
 """
     ComplexD64
@@ -37,37 +30,6 @@ Also the constructor for that struct.
 
 Union of FloatD64 and ComplexD64 types.
 """ FloatComplexD64
-
-"""
-   HiLo(x)
-
-Unwraps the two tuple: (most significant part, least significant part).
-
-see: [`Hi`](@ref), [`Lo`](@ref)
-""" HiLo
-
-"""
-   Hi(x)
-
-Unwraps the most significant part.
-
-see: [`Lo`](@ref), [`HiLo`](@ref)
-""" Hi
-
-"""
-   Lo(x)
-
-Unwraps the least signficant part.
-
-see: [`Hi`](@ref), [`HiLo`](@ref)
-""" Lo
-
-"""
-   ReIm(x::Complex)
-
-Unwraps real(x) and imag(x).
-
-""" ReIm
 
 """
    fastabs(x)
