ccea27f6d565ea35193fa62b90525487eba0ab30
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-11-18 01:03:25 -0500

    Create fast_minmax.jl

---
+++ src/support/fast_minmax.jl
@@ -0,0 +1,27 @@
+"""
+    unsafe_min(x, y)
+    unsafe_max(x, y)
+    
+Only safe for use where neither x nor y is NaN
+""" unsafe_min, unsafe_max
+
+unsafe_min(x::Float64, y::Float64) = signbit(x - y) ? x : y
+unsafe_max(x::Float64, y::Float64) = signbit(y - x) ? x : y
+
+function fast_min(x::Float64, y::Float64) 
+    x_minus_y = x - y
+    signbit(x_minus_y) ? x : ifelse(!isnan(x_minus_y), y, NaN64)
+end
+
+function fast_max(x::Float64, y::Float64) 
+    x_minus_y = x - y
+    signbit(x_minus_y) ? y : ifelse(!isnan(x_minus_y), x, NaN64)
+end
+
+function unsafe_minmax(x::T, y::T) where {T<:FastFloat} 
+    (unsafe_min(x,y), unsafe_max(x,y))
+end
+
+function unsafe_maxmin(x::T, y::T) where {T<:FastFloat}
+    (signbit(y-x) || isnan(x)) ? (x,y) : (y,x)
+end
