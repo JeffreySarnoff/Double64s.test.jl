0f062c960a6cad97a96d67f1ba19984ceaa9365e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-11-25 10:18:25 -0500

    Update exp.jl

--- src/math/elementary/exp.jl
+++ src/math/elementary/exp.jl
@@ -3,15 +3,7 @@
    The associaed functions `expm1(x)` and `expm1(z)`.
 =#
 
-const Ln2 = FloatD64((0.6931471805599453, 2.3190468138462996e-17))
-
-function exp_specialvalues(x::Float64)
-   isnan(x) && return x
-   absx = abs(x)
-   absx > 709.0 && return signbit(x) ? -InfD64 : InfD64
-   abx < 2.0^-257 && return one(FloatD64)
-   return nothing
-end
+const Ln2_D64 = FloatD64((0.6931471805599453, 2.3190468138462996e-17))
 
 function Base.exp(x::FloatD64)
    exp_specialvalues(Hi(x))
@@ -26,6 +18,14 @@ function Base.exp(x::FloatD64)
    # Determine r using the k we computed
    r = absx - (k * Ln2)
   
+end
+
+function exp_specialvalues(x::Float64)
+   isnan(x) && return x
+   absx = abs(x)
+   absx > 709.0 && return signbit(x) ? -InfD64 : InfD64
+   abx < 2.0^-257 && return one(FloatD64)
+   return nothing
 end
 
 #=
