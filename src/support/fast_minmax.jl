e3f3067749a2e3ea782ae8e944bac27eea722063
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-05-13 17:46:24 -0700

    Update fast_minmax.jl

--- src/support/fast_minmax.jl
+++ src/support/fast_minmax.jl
@@ -18,6 +18,9 @@ Only safe for use where neither x nor y is NaN
 unsafe_min(x::Float64, y::Float64) = signbit(x - y) ? x : y
 unsafe_max(x::Float64, y::Float64) = signbit(y - x) ? x : y
 
+unsafe_minmax(x::Float64, y::Float64) = ifelse(x < y, (x, y), (y, x))
+unsafe_maxmin(x::Float64, y::Float64) = ifelse(y < x, (x, y), (y, x))
+
 function fast_min(x::Float64, y::Float64) 
     x_minus_y = x - y
     signbit(x_minus_y) ? x : ifelse(!isnan(x_minus_y), y, NaN64)
@@ -26,14 +29,6 @@ end
 function fast_max(x::Float64, y::Float64) 
     x_minus_y = x - y
     signbit(x_minus_y) ? y : ifelse(!isnan(x_minus_y), x, NaN64)
-end
-
-function unsafe_minmax(x::Float64, y::Float64) 
-    (unsafe_min(x,y), unsafe_max(x,y))
-end
-
-function unsafe_maxmin(x::Float64, y::Float64)
-    (signbit(y-x) || isnan(x)) ? (x,y) : (y,x)
 end
 
 unsafe_minmag(x::Float64, y::Float64) = ifelse(abs(x) < abs(y), x, y)
